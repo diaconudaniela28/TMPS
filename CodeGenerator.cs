c00f3e57cd8aa6da30fdde5ac902fb8eeeceace1
diaconudaniela28 <89979866+diaconudaniela28@users.noreply.github.com>
2022-06-13 00:24:23 +0300

    Add files via upload

---
+++ CodeGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generator
+{
+    class CodeGenerator
+    {
+        private static CodeGenerator instance = null;
+        private static object locker = new object();
+
+        public int Number { get; set; }
+
+        private CodeGenerator()
+        {
+            Console.WriteLine("Generatorul de cod a fost creat");
+        }
+
+        public static CodeGenerator GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                        instance = new CodeGenerator();
+                }
+            }
+            return instance;
+        }
+
+        public void StartGenerate(string numberCode)
+        {
+            Console.WriteLine($"Utilizati codul: {numberCode}");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           CodeGenerator.GetInstance().StartGenerate("8272");
+        }
+    }
+}
